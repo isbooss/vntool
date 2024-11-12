1135aaf82a9b0e5bf88bdef1638cf6865734e627
isbooss <phonghai8928@gmail.com>
2024-11-13 01:53:21 +0700

    Update alexswann.py

--- alexswann.py
+++ alexswann.py
@@ -2,7 +2,7 @@ import requests
 from time import sleep
 BASE_URL: str = "https://cpmsalebot.ru/api"
 
-class vntool:
+class CPMtool:
 
     def __init__(self, access_key) -> None:
         self.auth_token = None
